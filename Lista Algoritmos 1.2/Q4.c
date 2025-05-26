931c668be46bedb393ab31051ef638686ac94df1
art-sec <arthurfoiceiro@gmail.com>
2025-05-26 19:30:58 -0300

    Q4 Resolvida

---
+++ Lista Algoritmos 1.2/Q4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int num1, num2;
+
+	printf("Insira o primeiro numero:\n");
+	scanf("%d", &num1);
+	printf("Insira o segundo numero:\n");
+	scanf("%d", &num2);
+
+	if (num1 > num2) {
+		printf("%d > %d", num1, num2);
+	}
+	else {
+		printf("%d > %d", num2, num1);
+	}
+}
